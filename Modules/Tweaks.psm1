982623c558b23e6a60b6a181d8ab05aa7f517c69
Will Bixler <william_bixler@yahoo.com>
2019-12-10 11:02:39 -0700

    Error Catching

--- Modules/Tweaks.psm1
+++ Modules/Tweaks.psm1
@@ -58,8 +58,14 @@ Function Tweaks {
     }
 
     $Console.AppendText("`r`nDisabling USB selective suspend")
-    powercfg /SETDCVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
-    powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+    try {
+        powercfg /SETDCVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+        powercfg /SETACVALUEINDEX SCHEME_CURRENT 2a737441-1930-4402-8d77-b2bebba308a3 48e6b7a6-50f5-4782-a5d4-53bb8f07e226 0
+        $Console.AppendText("`r`n`rSuccess")
+    }
+    catch {
+        $Console.AppendText("`r`n`rFailed")
+    }
 
     $Console.AppendText("`r`nDisabling network adapter power management...")
     $adapters = Get-NetAdapter -Physical | Get-NetAdapterPowerManagement
