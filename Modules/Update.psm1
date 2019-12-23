b167622e5cd85b1765078ba96d2e9dd06a4a2f9c
Will Bixler <william_bixler@yahoo.com>
2019-12-23 13:28:27 -0700

    Added temp git installer

--- Modules/Update.psm1
+++ Modules/Update.psm1
@@ -6,6 +6,8 @@ Function Update {
         $CurrentVersion
     )
 
+    $tempGit = $false
+
     $Console.Clear()
 
     $Console.AppendText("Updating...")
@@ -13,29 +15,45 @@ Function Update {
     $Console.AppendText("`r`n`r`nChecking Git installation...")
     $gitInstalled = Get-Command -ErrorAction SilentlyContinue git
     if (!$gitInstalled) {
-        $Console.AppendText("`r`nGit not installed... Cannot update.")
-        return
+        $Console.AppendText("`r`nGit not installed... Downloading...")
+
+        $tempGit = $true
+        $url = "https://github.com/git-for-windows/git/releases/download/v2.24.1.windows.2/Git-2.24.1.2-64-bit.exe"
+        $fileOut = "C:\SOFTWARE\Git.exe"
+
+        if (!(Get-Item "C:\SOFTWARE" -ErrorAction SilentlyContinue)) {
+            New-Item "C:\SOFTWARE" -ItemType Directory
+        }
+
+        if (!(Get-Item $fileOut -ErrorAction SilentlyContinue)) {
+            Invoke-WebRequest -Uri $url -OutFile $fileOut
+        }
+
+        Start-Process $fileOut /silent -Wait
     } else {
         $Console.AppendText("`r`nGit installed... Updating...")
     }
 
     Set-Location $ScriptRoot
     $Console.AppendText("`r`n`r`nFinding local changes...")
-    git add .
-    git commit -m "ff"
-    git fetch --all
-    git reset --hard origin/master
+    Start-Process "C:\Program Files\Git\git-cmd.exe" -ArgumentList { "git add . && git commit -m 'ff' && git fetch --all && git reset --hard origin/master && exit" } -Wait
     $Console.AppendText("`r`nFinding and installing updates...")
-    git pull
+    Start-Process "C:\Program Files\Git\git-cmd.exe" -ArgumentList { "git pull && exit" } -Wait
 
     $NewVersion = (Get-Content "$ScriptRoot\Info.json" | ConvertFrom-Json -ErrorAction SilentlyContinue).Version
+    
+    if ($tempGit) {
+        $Console.AppendText("`r`nRemoving temp git...")
+        $GitRegKey = Get-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*" | Where-Object { $_.DisplayName -like "Git*" }
+        Start-Process $GitRegKey.UninstallString /Silent -Wait
+    }
 
     $Console.AppendText("`r`n`r`nDone!")
     if ($CurrentVersion -like $NewVersion) {
         $Console.AppendText("`r`nNo updates found.")
         return $false
     } else {
-        $Console.AppendText("`r`nUpdated from $($CurrentVersion) to $($NewVersion)!")
+        $Console.AppendText("`r`n`r`nUpdated from $($CurrentVersion) to $($NewVersion)!")
         $Console.AppendText("`r`nRestarting in ");
         for($i = 5; $i -gt 0; $i--) {
             $Console.AppendText("$($i)... ")
