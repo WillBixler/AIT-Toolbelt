28bb722aa951e0ff3e5710881def799343a9f225
Will Bixler <william_bixler@yahoo.com>
2020-12-15 17:08:40 -0700

    Hide Local AIT Account

--- Modules/Tweaks.psm1
+++ Modules/Tweaks.psm1
@@ -271,6 +271,22 @@ Function Tweaks {
     $Console.AppendText("`r`nHiding Taskbar labels...")
     Set-ItemProperty -Path REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name NoTaskGrouping -Value 0
 
+    $Console.AppendText("`r`nHiding AIT Account...")
+    $AIT = Get-ItemProperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" | Select-Object AIT
+    if ($AIT -like "*0*") {
+        $Console.AppendText("`r`n`tAlready hidden")
+    } else {
+        try {
+            New-Item -Path "REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts" -ErrorAction SilentlyContinue
+            New-Item -Path "REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -ErrorAction SilentlyContinue
+            Set-ItemProperty -Path "REGISTRY::HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon\SpecialAccounts\UserList" -Name AIT -Value 0
+            $Console.AppendText("`r`n`tSuccess")
+        }
+        catch {
+            $Console.AppendText("`r`n`tFailed")
+        }
+    }
+
     $Console.AppendText("`r`n`r`nRestarting Windows Explorer...")
     Stop-Process -ProcessName explorer
 
